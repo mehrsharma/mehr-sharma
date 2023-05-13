abd13eb6cbee8fe70f2c01162c94e9b9d16fcde8
Mehr Sharma <mehrsharma@gmail.com>
2023-05-13 11:21:48 -0400

    updating resume

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import Container from './container'
 import { SocialIcon } from 'react-social-icons';
 import styles from './Title.module.css';
-import resume from './mehr-sharma-resume.pdf'
+// import resume from './mehr-sharma-resume.pdf'
 
 const Footer = () => {
   return (
@@ -13,7 +13,8 @@ const Footer = () => {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href={resume}
+              href="/mehr_sharma_resume.pdf"
+              target="_blank"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               resume
